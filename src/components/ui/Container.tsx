d984be89d01e7132b2f3dbdae4faa7e77c08691e
d-vale <vale.daniel@outlook.com>
2026-04-01 15:05:54 +0200

    feat(responsive): reduce Container padding on mobile

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -5,7 +5,7 @@ interface ContainerProps {
 
 export default function Container({ children, className = '' }: ContainerProps) {
   return (
-    <div className={`max-w-[1412px] mx-auto px-10 ${className}`}>
+    <div className={`max-w-[1412px] mx-auto px-4 md:px-6 lg:px-10 ${className}`}>
       {children}
     </div>
   )
